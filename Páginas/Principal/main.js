e719e76dd0f0b85c27ae06c8034d0176d02be416
Pablo Leon <100316797@alumnos.uc3m.es>
2016-04-29 13:05:50 +0200

    Footer terminado
    html, css e imágenes del footer (lo de main.js no sé qué es, lo abrí
    para ver cosas y lo dejé como estba, pero no le ha gustado a git)

--- Páginas/Principal/main.js
+++ Páginas/Principal/main.js
@@ -1,26 +1,26 @@
 $(document).ready(function () {
-     
+
     $(document).mouseup(function (e){
         var login = $("#login");
         var register = $("#register");
-        
+
         if (!login.is(e.target) && login.has(e.target).length == 0 && login.is(":visible")){
             login.hide();
             $("#header").css("filter", "none");
             $("#menu").css("filter", "none");
             $("#cuerpo").css("filter", "none");
         }
-        
+
         if (!register.is(e.target) && register.has(e.target).length == 0 && register.is(":visible")){
             register.hide();
             $("#header").css("filter", "none");
             $("#menu").css("filter", "none");
             $("#cuerpo").css("filter", "none");
         }
-        
+
     });
-    
-    
+
+
     var index = 0;								//index to determine the number of the slide which appears
     var lastIndex = 0;
     var slides = $(".slider div");				//slide which is in the div at the moment
@@ -105,7 +105,7 @@ function show_login() {
     $("#menu").css("filter", "blur(5px)");
     $("#cuerpo").css("filter", "blur(5px)");
 }
-    
+
 function show_register() {
     $("#login").hide();
     $("#register").show();
@@ -118,14 +118,14 @@ function show_register() {
 function validate_login() {
     var user = $("#usr_l");
     var password = $("#password_l");
-    
+
     if (user.val() == ""){
         user.css("border", "1px solid red");
     }
     else {
         user.css("border", "1px solid grey");
     }
-    
+
     if (password.val() == ""){
         password.css("border", "1px solid red");
     }

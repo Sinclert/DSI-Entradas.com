51913569a08bd8e26114e59598bd6bd697f2689a
Sinclert <Sinclert@hotmail.com>
2016-05-08 17:33:06 +0200

    Buy seat selection completed!! 🎉 🎉

--- Páginas/Compra/Compra.js
+++ Páginas/Compra/Compra.js
@@ -2,7 +2,9 @@
 	DSI PROJECT GROUP NUMBER 4
 */
 
-$(document).ready(function () {
+var price = 10;
+
+$(document).ready(function() {
     
     $("#datepicker").datepicker();
     
@@ -24,8 +26,83 @@ $(document).ready(function () {
             $("#cuerpo").css("filter", "none");
         }
         
-    }); 
+    });
+    
+    
+    /* SEATS SELECTION JAVASCRIPT */
+    
+	var $cart = $('#selected-seats'), //Sitting Area
+	$counter = $('#counter'), //Votes
+	$total = $('#total'); //Total money
+	
+	var sc = $('#seat-map').seatCharts({
+		map: [  //Seating chart
+			'aaaaaaaaaa',
+            'aaaaaaaaaa',
+            '__________',
+            'aaaaaaaa__',
+            'aaaaaaaaaa',
+			'aaaaaaaaaa',
+			'aaaaaaaaaa',
+			'aaaaaaaaaa',
+			'aaaaaaaaaa',
+            'aa__aa__aa'
+		],
+		naming : {
+			top : false,
+			getLabel : function (character, row, column) {
+				return column;
+			}
+		},
+		legend : { //Definition legend
+			node : $('#legend'),
+			items : [
+				[ 'a', 'available',   'Libre' ],
+				[ 'a', 'unavailable', 'Vendida']
+			]					
+		},
+		click: function () { //Click event
+			if (this.status() == 'available') { //optional seat
+				$('<li>R'+(this.settings.row+1)+' S'+this.settings.label+'</li>')
+					.attr('id', 'cart-item-'+this.settings.id)
+					.data('seatId', this.settings.id)
+					.appendTo($cart);
+
+				$counter.text(sc.find('selected').length+1);
+				$total.text(recalculateTotal(sc)+price);
+							
+				return 'selected';
+			} else if (this.status() == 'selected') { //Checked
+					//Update Number
+					$counter.text(sc.find('selected').length-1);
+					//update totalnum
+					$total.text(recalculateTotal(sc)-price);
+						
+					//Delete reservation
+					$('#cart-item-'+this.settings.id).remove();
+					//optional
+					return 'available';
+			} else if (this.status() == 'unavailable') { //sold
+				return 'unavailable';
+			} else {
+				return this.style();
+			}
+		}
+	});
+	//sold seat
+	sc.get(['1_2', '4_4','4_5','6_6','6_7','8_5','8_6','8_7','8_8', '10_1', '10_2']).status('unavailable');
+		
 });
+
+
+function recalculateTotal(sc) {
+	var total = 0;
+	sc.find('selected').each(function () {
+		total += price;
+	});
+			
+	return total;
+}
 
 
 function show_login() {
